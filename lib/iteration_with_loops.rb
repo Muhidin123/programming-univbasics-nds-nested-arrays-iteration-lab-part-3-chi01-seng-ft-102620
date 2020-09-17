6f9e7e6980d53370892620a8d92714165851c7c3
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-17 21:38:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,17 +1,15 @@
 def join_nested_strings(src)
   index = 0
   arr = []
-  arr_solution = []
   while index < src.count do
     inner_ix = 0
     while inner_ix < src[index].count do
       if src[index][inner_ix].class == String
         arr << src[index][inner_ix]
-        arr_solution = arr.join(" ")
       end
       inner_ix += 1
     end
     index += 1
   end
-  arr_solution
+  arr.join(" ")
 end
